ecfa768754424472b90c1fa10871de3077f1e38d
Kirill <kirill.simagin@orange.com>
2020-04-26 14:58:58 +0200

    initial

---
+++ YASL/YASL.h
@@ -0,0 +1,19 @@
+//
+//  YASL.h
+//  YASL
+//
+//  Created by KIRILL SIMAGIN on 26/04/2020.
+//  Copyright © 2020 Kirill. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YASL.
+FOUNDATION_EXPORT double YASLVersionNumber;
+
+//! Project version string for YASL.
+FOUNDATION_EXPORT const unsigned char YASLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YASL/PublicHeader.h>
+
+
